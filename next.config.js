97cea6cda5c7fa60e5bc90bf542937d6f14d7fc5
Jacob Harvey <jharvey@mahalobanking.com>
2024-01-14 18:52:45 -0700

    feat: update next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+
+const nextConfig = {
+    output: "export",
     experimental: {
         forceSwcTransforms: true,
     },
 };
+
+module.exports = nextConfig;
